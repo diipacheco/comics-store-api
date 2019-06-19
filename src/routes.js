72f84e5b0e75ab0f76503da09a006fd24cab500a
Edilson Pacheco <edilsopacheco6@gmail.com>
2019-06-19 18:21:02 -0300

    inserction of the authentication middleware in the routes of dashboard

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import ProductController from './controllers/ProductControllers'
 import AuthController from './controllers/AuthController'
 import multer from 'multer'
 import uploadConfig from './config/multerconfig/upload'
+import authMiddleware from './middlewares/auth'
 const upload = multer(uploadConfig)
 
 const routes = Router()
@@ -12,10 +13,28 @@ routes.post('/signup', AuthController.register)
 routes.post('/signin', AuthController.signin)
 
 // Products
-routes.get('/products', ProductController.index)
-routes.get('/products/:id', ProductController.show)
-routes.post('/products/new', upload.single('image'), ProductController.store)
-routes.put('/products/edit/:id', ProductController.update)
-routes.delete('/products/delete/:id', ProductController.delete)
+routes.get('/dashboard/products', authMiddleware, ProductController.index)
+routes.get('/dashboard/products/:id', authMiddleware, ProductController.show)
+routes.post(
+  '/dashboard/products/new',
+  authMiddleware,
+  upload.single('image'),
+
+  ProductController.store
+)
+
+routes.put(
+  '/dashboard/products/edit/:id',
+  authMiddleware,
+
+  ProductController.update
+)
+
+routes.delete(
+  '/dashboard/products/delete/:id',
+  authMiddleware,
+
+  ProductController.delete
+)
 
 export default routes
