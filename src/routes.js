0ed9dc5c4a1c2ca71d2a4a38fbcd25deccfc41de
Edilson Pacheco <edilsopacheco6@gmail.com>
2019-06-21 17:01:16 -0300

    route to reset the password

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ const routes = Router()
 // User
 routes.post('/signup', AuthController.register)
 routes.post('/signin', AuthController.signin)
+routes.post('/auth/forgot_password', AuthController.forgotPassword)
 
 // Products
 routes.get('/dashboard/products', authMiddleware, ProductController.index)
